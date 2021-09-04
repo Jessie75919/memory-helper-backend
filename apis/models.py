3970bb2fc3553efe5fdbdd8565b43122571a4537
jessie75919 <jessie75919@gmail.com>
2021-09-04 13:13:19 +0800

    chore: modify for models, serializers

--- apis/models.py
+++ apis/models.py
@@ -3,8 +3,7 @@ from django.db import models
 
 class Stage(models.Model):
     level = models.PositiveSmallIntegerField(default=0)
-    next_stage = models.ForeignKey('Stage', on_delete=models.SET_NULL, null=True)
-    time_amount = models.DecimalField(max_digits=5, decimal_places=2)
+    time_amount = models.IntegerField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
@@ -16,18 +15,22 @@ class Question(models.Model):
     content = models.CharField(max_length=255)
     is_visible = models.BooleanField(default=True)
     count_of_seen = models.PositiveSmallIntegerField(default=1)
-    last_seen_at = models.DateTimeField(auto_now=True)
+    last_seen_at = models.DateTimeField(null=True)
+    next_show_at = models.DateTimeField(null=True)
     completed_at = models.DateTimeField(null=True)
     stage = models.ForeignKey(Stage, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"content: {self.content}, stage_id: {self.stage}"
+        return f"content: {self.content}, " \
+               f"stage_id: {self.stage},  " \
+               f"last_seen_at: {self.last_seen_at}, " \
+               f"next_show_at: {self.next_show_at}"
 
 
 class Answer(models.Model):
-    content = models.TextField()
+    content = models.TextField(blank=True, null=True)
     question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
--- apis/serializers.py
+++ apis/serializers.py
@@ -1,3 +1,5 @@
+import datetime
+
 from rest_framework import serializers
 
 from apis.models import Question, Answer, Stage
@@ -6,7 +8,7 @@ from apis.models import Question, Answer, Stage
 class StageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Stage
-        fields = ('id', 'level', 'time_amount', 'next_stage')
+        fields = ('id', 'level', 'time_amount', 'next_stage_level')
 
 
 class AnswerSerializer(serializers.ModelSerializer):
@@ -20,11 +22,23 @@ class QuestionSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         answers_data = validated_data.pop('answers')
-        stage_id = validated_data.pop('stage')
-        question = Question.objects.create(**validated_data, stage=stage_id)
+        validated_data['last_seen_at'] = datetime.datetime.now()
+        validated_data['next_show_at'] = datetime.datetime.now()
+        question = Question.objects.create(**validated_data, stage_id=1)
         for answer_data in answers_data:
             Answer.objects.create(question=question, **answer_data)
         return question
+
+    def update(self, instance, validated_data):
+        instance.content = validated_data['content']
+        instance.save()
+        answers_data = validated_data.pop('answers')
+        for answer_data in answers_data:
+            ans = Answer.objects.filter(question_id=instance.id).update(**answer_data)
+            print(ans)
+
+        return instance
+
 
     class Meta:
         model = Question
